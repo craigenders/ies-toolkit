2d38d700bf9c43c921a39277d18620536e61662d
Tonatiuhxotl <41914393+Tonatiuhxotl@users.noreply.github.com>
2025-07-04 16:19:23 -0700

    Ex1.2

--- Ex1. FIML Linear Regression/Ex1.2.R
+++ Ex1. FIML Linear Regression/Ex1.2.R
@@ -2,16 +2,15 @@
 # fiml linear regression
 ##################################################
 
-library(fdir)
 library(lavaan)
 library(semTools)
 
 ##################################################
-# set working directory and load data
+# load data from github
 ##################################################
 
-set()
-load('behaviorachievement.rda')
+data_url <- "https://raw.githubusercontent.com/craigenders/ies-toolkit/main/Data/behaviorachievement.rda"
+load(gzcon(url(data_url, open = "rb")))
 
 ##################################################
 # analysis
@@ -20,12 +19,12 @@ load('behaviorachievement.rda')
 # estimate model in lavaan
 model <- 'read9 ~ b1*read1 + b2*lrnprob1 + b3*behsymp1'
 fit <- sem.auxiliary(model, behaviorachievement, fixed.x = F, aux = c("hispanic","read2","stanread7"))
+# missing data patterns and proportion observed data (coverage)
+inspect(fit, "patterns")
+inspect(fit, "coverage")
 summary(fit, rsquare = T, standardize = T)
 
 # wald test that all slopes equal 0
 wald.constraints <- 'b1 == 0; b2 == 0; b3 == 0;'
 lavTestWald(fit, constraints = wald.constraints)
 
-# missing data patterns and proportion observed data (coverage)
-inspect(fit, "patterns")
-inspect(fit, "coverage")
