fde0d0ceab4c22fe83795712d85e775f83b7fd27
Tonatiuhxotl <41914393+Tonatiuhxotl@users.noreply.github.com>
2025-07-04 15:06:09 -0700

    Chapter1
    Small changes for the R code for Ex1.1 and Ex1.2

--- Ex1. FIML Linear Regression/Ex1.1.R
+++ Ex1. FIML Linear Regression/Ex1.1.R
@@ -2,15 +2,14 @@
 # fiml linear regression
 ##################################################
 
-library(fdir)
 library(lavaan)
 
 ##################################################
-# set working directory and load data
+# load data from github
 ##################################################
 
-set()
-load('behaviorachievement.rda')
+data_url <- "https://raw.githubusercontent.com/craigenders/ies-toolkit/main/Data/behaviorachievement.rda"
+load(gzcon(url(data_url, open = "rb")))
 
 ##################################################
 # analysis
@@ -19,6 +18,7 @@ load('behaviorachievement.rda')
 # estimate model in lavaan
 model <- 'read9 ~ b1*read1 + b2*lrnprob1 + b3*behsymp1'
 fit <- sem(model, behaviorachievement, fixed.x = F, missing = "fiml")
+# missing data patterns and proportion observed data (coverage)
 inspect(fit, "patterns")
 inspect(fit, "coverage")
 summary(fit, rsquare = T, standardize = T)
@@ -27,6 +27,5 @@ summary(fit, rsquare = T, standardize = T)
 wald.constraints <- 'b1 == 0; b2 == 0; b3 == 0;'
 lavTestWald(fit, constraints = wald.constraints)
 
-# missing data patterns and proportion observed data (coverage)
-inspect(fit, "patterns")
-inspect(fit, "coverage")
+
+
